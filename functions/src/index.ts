60e32d09ac75016b24210d6264c7b80472da4476
Jacopo <179255721+jackc1313@users.noreply.github.com>
2026-04-25 11:52:29 +0200

    fix: add missing Cloud Function exports to index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,3 +3,5 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export { generateWeeklyHoroscopes } from "./generateHoroscopes";
+export { generateDailyContent } from "./generateDailyContent";
+export { generateTarotInterpretations } from "./generateTarot";
